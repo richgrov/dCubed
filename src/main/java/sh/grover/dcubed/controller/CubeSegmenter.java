a0347132db0025d688b933a6ef929aae5dd08124
unknown <38992201+richgrov@users.noreply.github.com>
2024-02-04 22:25:20 -0700

    Segment only for black backgrounds

--- src/main/java/sh/grover/dcubed/controller/CubeSegmenter.java
+++ src/main/java/sh/grover/dcubed/controller/CubeSegmenter.java
@@ -11,10 +11,10 @@ public class CubeSegmenter {
     private static final Size GAUSSIAN_KERNEL_SIZE = new Size(5, 3);
     private static final Scalar CONTOUR_COLOR1 = new Scalar(255, 255, 255);
 
-    private final Mat intermediate = new Mat();
-    private final Mat intermediateCanny = new Mat();
+    private final Mat hsv = new Mat();
+    private final Mat foregroundSegment = new Mat();
+    private final Mat blurredForeground = new Mat();
     private final Mat hierarchyUnused = new Mat();
-    private final Mat maskOutput = new Mat();
     private final List<MatOfPoint> contourPoints = new ArrayList<>();
     private final List<MatOfPoint> filteredContours = new ArrayList<>();
 
@@ -24,73 +24,44 @@ public class CubeSegmenter {
     }
 
     private Mat segmentCube(Mat input) {
-        var outlined = this.outlineFrame(input);
-        var contours = this.findMajorContours(outlined);
-        if (contours.isEmpty()) {
-            System.out.println("empty");
-            return outlined;
-        }
-
-        var masked = this.maskFromContours(input, contours);
-
-        var kernel = Imgproc.getStructuringElement(Imgproc.MORPH_RECT, new Size(15, 15));
-        Imgproc.erode(masked, masked, kernel);
-        Imgproc.cvtColor(masked, masked, Imgproc.COLOR_BGR2GRAY);
-        var contours2 = this.findMajorContours(masked);
-        var approxCurves = new ArrayList<MatOfPoint>();
-        for (var contour : contours2) {
-            var approx = new MatOfPoint2f();
-            Imgproc.approxPolyDP(new MatOfPoint2f(contour.toArray()), approx, 15, true);
-
-            var points = approx.toArray();
-            if (points.length == 6) {
-                approxCurves.add(new MatOfPoint(points));
-            }
-        }
-
-        if (approxCurves.size() == 1) {
-            Imgproc.drawContours(input, approxCurves, -1, CONTOUR_COLOR1, 3);
-        }
-
+        var foreground = this.segmentForeground(input);
+        this.contour(foreground);
+        Imgproc.drawContours(input, this.filteredContours, -1, CONTOUR_COLOR1, 3);
         return input;
     }
 
-    private Mat outlineFrame(Mat input) {
-        Imgproc.medianBlur(input, this.intermediate, 15);
-        Imgproc.Canny(this.intermediate, this.intermediateCanny, 20, 30);
-        var kernel = Imgproc.getStructuringElement(Imgproc.MORPH_RECT, new Size(15, 15));
-        Imgproc.dilate(this.intermediateCanny, this.intermediateCanny, kernel);
-        return this.intermediateCanny;
+    private Mat segmentForeground(Mat input) {
+        Imgproc.cvtColor(input, this.hsv, Imgproc.COLOR_BGR2HSV);
+        Core.inRange(this.hsv, new Scalar(0, 0, 50), new Scalar(255, 255, 255), this.foregroundSegment);
+        Imgproc.medianBlur(this.foregroundSegment, this.blurredForeground, 11);
+        return this.blurredForeground;
     }
 
-    private List<MatOfPoint> findMajorContours(Mat input) {
+    private void contour(Mat input) {
         this.contourPoints.clear();
         Imgproc.findContours(input, this.contourPoints, this.hierarchyUnused, Imgproc.RETR_EXTERNAL, Imgproc.CHAIN_APPROX_SIMPLE);
 
         this.filteredContours.clear();
         for (var contour : this.contourPoints) {
-            if (isValidContour(contour)) {
-                this.filteredContours.add(contour);
+            var points = contour.toArray();
+            if (points.length < 6) {
+                continue;
             }
+
+            var approx = new MatOfPoint2f();
+            Imgproc.approxPolyDP(new MatOfPoint2f(contour.toArray()), approx, 15, true);
+
+            var area = Imgproc.contourArea(contour);
+            if (area < 10000) {
+                continue;
+            }
+
+            var smoothPoints = approx.toArray();
+            if (smoothPoints.length != 6) {
+                continue;
+            }
+
+            this.filteredContours.add(new MatOfPoint(smoothPoints));
         }
-
-        return this.filteredContours;
-    }
-
-    private static boolean isValidContour(MatOfPoint contour) {
-        var points = contour.toArray();
-        if (points.length < 6) {
-            return false;
-        }
-
-        var area = Imgproc.contourArea(contour);
-        return area > 5000;
-    }
-
-    private Mat maskFromContours(Mat image, List<MatOfPoint> contours) {
-        var mask = Mat.zeros(image.size(), CvType.CV_8UC(image.channels()));
-        Imgproc.drawContours(mask, contours, -1, CONTOUR_COLOR1, -1);
-        Core.bitwise_and(image, mask, this.maskOutput);
-        return this.maskOutput;
     }
 }
