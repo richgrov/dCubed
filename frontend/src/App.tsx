8354d6b73835ca4406c28a3162fc1b39a999e15f
unknown <38992201+richgrov@users.noreply.github.com>
2024-02-14 19:29:37 -0700

    Initial frontend design

---
+++ frontend/src/App.tsx
@@ -0,0 +1,15 @@
+import Hero from "./Hero";
+
+export default function App() {
+  return (
+    <>
+      <Hero />
+      <div>
+        <canvas />
+      </div>
+      <div>
+        <div></div>
+      </div>
+    </>
+  );
+}
---
+++ frontend/src/Hero.tsx
@@ -0,0 +1,75 @@
+import wavyCircle from "./assets/circle.png";
+import cube from "./assets/cube.png";
+
+export default function Hero() {
+  return (
+    <div className="flex justify-center bg-amber-400 bg-[url('dots.png')] bg-contain bg-right bg-no-repeat py-48">
+      <div className="flex flex-col gap-10 font-['CalSans']">
+        <div>
+          <h1 className="text-stroke-black text-7xl text-transparent">
+            Solve your Rubik's Cube
+          </h1>
+          <h1 className="text-7xl">With Only 2 Photos</h1>
+        </div>
+        <div>
+          <p className="inline rounded-full border-2 border-black p-5">
+            No sign-up necessary.
+          </p>
+        </div>
+      </div>
+
+      <div className="relative self-center p-2">
+        <img
+          src={wavyCircle}
+          alt="Wavy Circle"
+          className="relative top-0 animate-[spin_30s_linear_infinite]"
+        />
+        <img src={cube} alt="Rubik's Cube" className="absolute top-0" />
+
+        <svg
+          width="100"
+          height="100"
+          xmlns="http://www.w3.org/2000/svg"
+          className="absolute top-[-150px]"
+        >
+          <circle
+            width="100"
+            height="100"
+            r="45"
+            cx="50"
+            cy="50"
+            stroke="black"
+            strokeWidth={5}
+            fill="transparent"
+          />
+        </svg>
+
+        <svg
+          width="100"
+          height="100"
+          xmlns="http://www.w3.org/2000/svg"
+          className="absolute bottom-[-150px] right-[-50px]"
+        >
+          <rect
+            width="100"
+            height="100"
+            x="0"
+            y="0"
+            rx="5"
+            ry="5"
+            fill="black"
+          />
+        </svg>
+
+        <svg
+          width="100"
+          height="100"
+          xmlns="http://www.w3.org/2000/svg"
+          className="absolute bottom-0 left-[-50px]"
+        >
+          <polygon points="0,0 100,100, 0,100" />
+        </svg>
+      </div>
+    </div>
+  );
+}
