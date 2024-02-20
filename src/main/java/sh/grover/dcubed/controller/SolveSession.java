a32977838aded18369586e0dd9274a860e8a437a
unknown <38992201+richgrov@users.noreply.github.com>
2024-02-20 12:13:52 -0700

    Use EnumMap for SolveSession

--- src/main/java/sh/grover/dcubed/controller/SolveSession.java
+++ src/main/java/sh/grover/dcubed/controller/SolveSession.java
@@ -4,12 +4,12 @@ import sh.grover.dcubed.controller.vision.ScannedSide;
 import sh.grover.dcubed.model.FaceColor;
 
 import java.util.Collections;
-import java.util.HashMap;
+import java.util.EnumMap;
 import java.util.Map;
 
 public class SolveSession {
 
-    private final HashMap<FaceColor, FaceColor[]> sides = new HashMap<>();
+    private final Map<FaceColor, FaceColor[]> sides = new EnumMap<>(FaceColor.class);
 
     public void addSide(ScannedSide side) {
         this.sides.put(side.sideColor(), side.faces());
