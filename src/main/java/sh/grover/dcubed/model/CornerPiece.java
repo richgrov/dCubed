03316889872bb666f605bb11d43e7760aba0e530
unknown <38992201+richgrov@users.noreply.github.com>
2024-02-22 21:46:17 -0700

    Make CornerPiece#equals ignore order

--- src/main/java/sh/grover/dcubed/model/CornerPiece.java
+++ src/main/java/sh/grover/dcubed/model/CornerPiece.java
@@ -7,4 +7,15 @@ public record CornerPiece(int color1, int color2, int color3) {
         FaceColor.requireValid(color2);
         FaceColor.requireValid(color3);
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof CornerPiece other)) {
+            return false;
+        }
+
+        var compare1 = (1 << this.color1) | (1 << this.color2) | (1 << this.color3);
+        var compare2 = (1 << other.color1) | (1 << other.color2) | (1 << other.color3);
+        return compare1 == compare2;
+    }
 }
