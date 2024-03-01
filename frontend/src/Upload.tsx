1ec693675d0029460121304ff4b335cf330f3f07
unknown <38992201+richgrov@users.noreply.github.com>
2024-03-01 15:49:18 -0700

    Report upload errors to the user

--- frontend/src/Upload.tsx
+++ frontend/src/Upload.tsx
@@ -7,8 +7,13 @@ import { Cube } from "./model";
 import phone from "./assets/phone.png";
 import loadingAnimation from "./assets/loading.gif";
 
+const INITIAL_INSTRUCTION = "";
+
+const NETWORK_ERROR = "Network error. Check connection and try again.";
 const SCAN_ERROR =
 	"Couldn't scan a cube in this image. Ensure it is centered in the frame and has 3 sides clearly visible.";
+const UPLOAD_ERROR =
+	"Wrong image upload. Double-check file type and try again.";
 const SERVER_ERROR = "Internal error. Please try again later.";
 
 export type CompletionCallback = (cube: Cube, sessionId: string) => void;
@@ -30,31 +35,39 @@ async function scan(
 		url += "?session=" + encodeURIComponent(sessionId);
 	}
 
+	let response: Response;
 	try {
-		const response = await fetch(url, { method: "POST", body: formData });
-		if (response.status === 422) {
-			throw new Error(SCAN_ERROR);
-		}
-
-		if (response.status !== 200) {
-			throw new Error(SERVER_ERROR);
-		}
-
-		const responseData = await response.json();
-
-		return {
-			cube: responseData.sides,
-			sessionId: responseData.sessionId,
-		};
-	} catch {
-		throw new Error("Network error. Check connection and try again.");
+		response = await fetch(url, { method: "POST", body: formData });
+	} catch (e) {
+		console.error("Fetch error: " + e);
+		throw new Error(NETWORK_ERROR);
 	}
+
+	if (response.status === 422) {
+		throw new Error(SCAN_ERROR);
+	}
+
+	if (response.status === 400) {
+		throw new Error(UPLOAD_ERROR);
+	}
+
+	if (response.status !== 200) {
+		throw new Error(SERVER_ERROR);
+	}
+
+	const responseData = await response.json();
+
+	return {
+		cube: responseData.sides,
+		sessionId: responseData.sessionId,
+	};
 }
 
 export default function Uploader(props: { onComplete: CompletionCallback }) {
 	const [loading, setLoading] = useState(false);
 	const [sides, setSides] = useState<Partial<Cube>>({});
 	const sessionId = useRef<string | undefined>();
+	const [errorMessage, setErrorMessage] = useState("");
 
 	const onDrop = useCallback(async (accepted: File[]) => {
 		setLoading(true);
@@ -67,6 +80,8 @@ export default function Uploader(props: { onComplete: CompletionCallback }) {
 			setSides((sides) => {
 				return { ...sides, ...result.cube };
 			});
+		} catch (e: any) {
+			setErrorMessage(e.message);
 		} finally {
 			setLoading(false);
 		}
@@ -92,16 +107,22 @@ export default function Uploader(props: { onComplete: CompletionCallback }) {
 		<div className="flex h-full flex-col items-center">
 			<div
 				{...getRootProps()}
-				className="flex w-full flex-1 cursor-pointer flex-col items-center justify-center gap-5 border-b-4 border-dashed border-black"
+				className="flex w-full flex-1 cursor-pointer flex-col items-center justify-center gap-5 border-b-4 border-dashed border-black text-center"
 			>
 				<input {...getInputProps()} />
+
+				{errorMessage !== "" && (
+					<div className="max-w-lg rounded-xl border-4 border-red-600 text-3xl text-red-600">
+						{errorMessage}
+					</div>
+				)}
 
 				<img
 					src={phone}
 					alt="A picture of a Rubik's cube on a flat surface"
 					className="rounded-xl border-4 border-dashed border-black"
 				/>
-				<p className="max-w-lg text-center text-2xl">
+				<p className="max-w-lg text-2xl">
 					Photograph a picture of your cube on a flat surface, where 3
 					sides are clearly visible.
 				</p>
