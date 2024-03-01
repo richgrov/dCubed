3fee2983ea6a8954789342a6e1e445a7d09795c7
unknown <38992201+richgrov@users.noreply.github.com>
2024-02-29 19:49:53 -0700

    Increase median line detection distance

--- src/main/java/sh/grover/dcubed/controller/vision/PhotoColorIdentifier.java
+++ src/main/java/sh/grover/dcubed/controller/vision/PhotoColorIdentifier.java
@@ -23,7 +23,7 @@ public class PhotoColorIdentifier implements IColorIdentifier {
     private static final Size GAUSSIAN_BLUR_KSIZE = new Size(3, 3);
     private static final Mat DILATION_KERNEL = Imgproc.getStructuringElement(1, new Size(3, 3));
     private static final double LINE_SEGMENTATION_DEVIATION = 20;
-    private static final int NEAR_LINE_DISTANCE = 10;
+    private static final int NEAR_LINE_DISTANCE = 20;
 
     private final ICubeSegmenter segmenter;
     private final StepDebugLevel debugLevel;
