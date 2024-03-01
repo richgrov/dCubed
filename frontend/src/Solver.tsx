13fec219a0d402dfaeeb9de4e3c1dd5ffdaf731c
Richard Grover <38992201+richgrov@users.noreply.github.com>
2024-03-01 15:56:21 -0700

    Improve user experience for uploading files (#31)
    * Add UI to explain cube framing
    
    * Extract upload component to its own file
    
    * Report upload errors to the user
    
    * Send proper response code for scan failure
    
    * Change error wording
    
    * Change error dialog style

--- frontend/src/Solver.tsx
+++ frontend/src/Solver.tsx
@@ -1,97 +1,28 @@
-import { useCallback, useRef, useState } from "react";
-import { Button } from "./Button";
-import { useDropzone } from "react-dropzone";
+import { useState } from "react";
 
-import loadingAnimation from "./assets/loading.gif";
-import Visual, { CubeInfo } from "./Visual";
+import Visual from "./Visual";
+import Uploader from "./Upload";
+import { AppState, Cube } from "./model";
 
 export default function Solver() {
-  const sessionId = useRef<string | undefined>(undefined);
-  const [loading, setLoading] = useState(false);
-  const [cubeInfo, setCubeInfo] = useState<CubeInfo | undefined>(undefined);
+  const [appState, setAppState] = useState<AppState | undefined>(undefined);
 
-  const onPhoto = async (file: File) => {
-    setLoading(true);
-
-    const formData = new FormData();
-    formData.append("photo", file);
-
-    let url = import.meta.env.VITE_BACKEND_URL + "/scan-photo";
-    if (typeof sessionId.current !== "undefined") {
-      url += "?" + new URLSearchParams({ session: sessionId.current });
-    }
-
-    try {
-      const response = await fetch(url, { method: "POST", body: formData });
-      if (response.status !== 200) {
-        return;
-      }
-
-      const responseData = await response.json();
-      sessionId.current = responseData.sessionId;
-
-      if (Object.keys(responseData.sides).length === 6) {
-        setCubeInfo({
-          sides: responseData.sides,
-          session: sessionId.current!,
-        });
-      }
-    } finally {
-      setLoading(false);
-    }
+  const onScanComplete = (scannedCube: Cube, session: string) => {
+    setAppState({
+      cube: scannedCube,
+      sessionId: session,
+    });
   };
 
   return (
     <div className="flex flex-col items-center gap-20 py-20">
-      <h1 className="text-5xl">Step 1: Scan Cube</h1>
+      <h1 className="text-5xl">Click or Drop File Below to Scan</h1>
       <div className="relative h-[80vh] w-4/5 rounded-2xl border-4 border-black">
-        {typeof cubeInfo !== "undefined" ? (
-          <Visual cubeInfo={cubeInfo} />
+        {typeof appState !== "undefined" ? (
+          <Visual appState={appState} />
         ) : (
-          <InputSetup onPhoto={onPhoto} />
+          <Uploader onComplete={onScanComplete} />
         )}
-        {loading ? (
-          <div className="rounded-1xl absolute bottom-0 left-0 right-0 top-0 flex items-center justify-center bg-[rgba(0,_0,_0,_0.5)]">
-            <img src={loadingAnimation} alt="Loading Animation" />
-          </div>
-        ) : (
-          ""
-        )}
-      </div>
-    </div>
-  );
-}
-
-function InputSetup(props: { onPhoto: (file: File) => void }) {
-  const onDrop = useCallback((accepted: File[]) => {
-    props.onPhoto(accepted[0]);
-  }, []);
-
-  const handleCamera = () => {};
-  const { getRootProps, getInputProps, isDragActive } = useDropzone({
-    accept: {
-      "image/png": [".png"],
-      "image/jpeg": [".jpeg"],
-    },
-    onDrop,
-    maxFiles: 1,
-  });
-
-  return (
-    <div className="flex h-full flex-col items-center">
-      <div
-        {...getRootProps()}
-        className="flex w-full flex-1 cursor-pointer items-center justify-center border-b-4 border-dashed border-black"
-      >
-        <input {...getInputProps()} />
-        <h1 className="text-3xl">
-          {isDragActive
-            ? "Drop Files Here"
-            : "Click to Upload or Drop Photo Here"}
-        </h1>
-      </div>
-      <div className="p-5">
-        <Button onClick={handleCamera}>Use Camera Instead</Button>
       </div>
     </div>
   );
---
+++ frontend/src/Upload.tsx
@@ -0,0 +1,142 @@
+import { useCallback, useEffect, useRef, useState } from "react";
+import { Button } from "./Button";
+import { useDropzone } from "react-dropzone";
+
+import { Cube } from "./model";
+
+import phone from "./assets/phone.png";
+import loadingAnimation from "./assets/loading.gif";
+
+const INITIAL_INSTRUCTION = "";
+
+const NETWORK_ERROR = "Network error. Check connection and try again.";
+const SCAN_ERROR =
+	"Couldn't find a cube in this image. Ensure it is centered in the frame and has 3 sides clearly visible.";
+const UPLOAD_ERROR =
+	"Wrong image upload. Double-check file type and try again.";
+const SERVER_ERROR = "Internal error. Please try again later.";
+
+export type CompletionCallback = (cube: Cube, sessionId: string) => void;
+
+type ScanResponse = {
+	cube: Partial<Cube>;
+	sessionId: string;
+};
+
+async function scan(
+	file: File,
+	sessionId: string | undefined
+): Promise<ScanResponse> {
+	const formData = new FormData();
+	formData.append("photo", file);
+
+	let url = import.meta.env.VITE_BACKEND_URL + "/scan-photo";
+	if (typeof sessionId !== "undefined") {
+		url += "?session=" + encodeURIComponent(sessionId);
+	}
+
+	let response: Response;
+	try {
+		response = await fetch(url, { method: "POST", body: formData });
+	} catch (e) {
+		console.error("Fetch error: " + e);
+		throw new Error(NETWORK_ERROR);
+	}
+
+	if (response.status === 422) {
+		throw new Error(SCAN_ERROR);
+	}
+
+	if (response.status === 400) {
+		throw new Error(UPLOAD_ERROR);
+	}
+
+	if (response.status !== 200) {
+		throw new Error(SERVER_ERROR);
+	}
+
+	const responseData = await response.json();
+
+	return {
+		cube: responseData.sides,
+		sessionId: responseData.sessionId,
+	};
+}
+
+export default function Uploader(props: { onComplete: CompletionCallback }) {
+	const [loading, setLoading] = useState(false);
+	const [sides, setSides] = useState<Partial<Cube>>({});
+	const sessionId = useRef<string | undefined>();
+	const [errorMessage, setErrorMessage] = useState("");
+
+	const onDrop = useCallback(async (accepted: File[]) => {
+		setLoading(true);
+		const file = accepted[0];
+
+		try {
+			const result = await scan(file, sessionId.current);
+			sessionId.current = result.sessionId;
+
+			setSides((sides) => {
+				return { ...sides, ...result.cube };
+			});
+		} catch (e: any) {
+			setErrorMessage(e.message);
+		} finally {
+			setLoading(false);
+		}
+	}, []);
+
+	useEffect(() => {
+		if (Object.keys(sides).length === 6) {
+			props.onComplete(sides as Cube, sessionId.current!);
+		}
+	}, [sides]);
+
+	const handleCamera = () => {};
+	const { getRootProps, getInputProps } = useDropzone({
+		accept: {
+			"image/png": [".png"],
+			"image/jpeg": [".jpeg"],
+		},
+		onDrop,
+		maxFiles: 1,
+	});
+
+	return (
+		<div className="flex h-full flex-col items-center">
+			<div
+				{...getRootProps()}
+				className="flex w-full flex-1 cursor-pointer flex-col items-center justify-center gap-5 border-b-4 border-dashed border-black text-center"
+			>
+				<input {...getInputProps()} />
+
+				{errorMessage !== "" && (
+					<div className="mb-5 max-w-3xl rounded-xl bg-red-600 p-5 text-2xl text-white shadow-[black_10px_10px]">
+						{errorMessage}
+					</div>
+				)}
+
+				<img
+					src={phone}
+					alt="A picture of a Rubik's cube on a flat surface"
+					className="rounded-xl border-4 border-dashed border-black"
+				/>
+				<p className="max-w-lg text-2xl">
+					Photograph a picture of your cube on a flat surface, where 3
+					sides are clearly visible.
+				</p>
+			</div>
+			<div className="p-5">
+				<Button onClick={handleCamera}>Use Camera Instead</Button>
+			</div>
+			{loading ? (
+				<div className="rounded-1xl absolute bottom-0 left-0 right-0 top-0 flex items-center justify-center bg-[rgba(0,_0,_0,_0.5)]">
+					<img src={loadingAnimation} alt="Loading Animation" />
+				</div>
+			) : (
+				""
+			)}
+		</div>
+	);
+}
