a60fb00cf4c5faabf520013eb9e5ac3129f653c8
Richard Grover <38992201+richgrov@users.noreply.github.com>
2024-03-01 17:57:54 -0700

    Add color scan indicators (#32)

--- frontend/src/model.ts
+++ frontend/src/model.ts
@@ -1,4 +1,6 @@
+export const COLORS = ["WHITE", "RED", "ORANGE", "YELLOW", "GREEN", "BLUE"];
 export type Color = "WHITE" | "RED" | "ORANGE" | "YELLOW" | "GREEN" | "BLUE";
+
 export type Cube = { [K in Color]: Color[] };
 
 export type AppState = {
